d4e1bd744cdc921a79fd2989e5b19b63fe3a5573
marciocaprioli <97766423+marciocaprioli@users.noreply.github.com>
2024-11-20 22:39:38 -0300

    Add files via upload

---
+++ Engenharia de Dados/Live 2.sqlite.sql
@@ -0,0 +1,24 @@
+-- TABLE
+CREATE TABLE cidade (
+  cod_cidade integer not NULL primary key, 
+  cidade varchar(20), 
+  uf varchar(2)
+);
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE depe (
+  prefixo integer not NULL primary key,
+  nome varchar(20), 
+  cod_cidade integer
+);
+CREATE TABLE funci (
+  matricula varchar(8) primary key, 
+  nome varchar(60), 
+  prefixo integer
+);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
